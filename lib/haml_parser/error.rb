f7e661b4e0735f0d32078d3479f8a424e65deefb
Kohei Suzuki <eagletmt@gmail.com>
2015-09-12 16:28:51 +0900

    Import parsers from faml

---
+++ lib/haml_parser/error.rb
@@ -0,0 +1,10 @@
+module HamlParser
+  class Error < StandardError
+    attr_accessor :lineno
+
+    def initialize(message, lineno)
+      super(message)
+      @lineno = lineno
+    end
+  end
+end
